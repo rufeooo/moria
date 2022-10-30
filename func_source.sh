89dd2c9d0eaf0159a25d8de958136b50de37f3cb
Alan Newton <rufeooo@gmail.com>
2022-10-30 09:00:39 -0700

    func_source.sh: exact match falls back to any string match

--- func_source.sh
+++ func_source.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-ADDR=0x`nm moria | grep "\<$1\>" | cut -f 1 -d ' '`
+ADDR=`nm moria | grep "\<$1\>" | cut -f 1 -d ' '`
+[ -z "$ADDR" ] && ADDR=`nm moria | grep $1 | cut -f 1 -d ' '`
 echo $ADDR
-IFS=":" read -a INF <<< `addr2line -e moria $ADDR`
+IFS=":" read -a INF <<< `addr2line -e moria 0x$ADDR`
 
 echo ${INF[@]}
 if [ ${#INF[@]} -gt 0 ]; then
