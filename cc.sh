7b24361d185628dbad0a00a9215cf97c9bd609f9
Alan Newton <rufeooo@gmail.com>
2022-11-21 19:00:58 -0800

    cc.sh: quote vars

--- cc.sh
+++ cc.sh
@@ -3,13 +3,13 @@
 
 hash $CC || exit 1
 
-[ -z $CFLAGS] && CFLAGS="-O1 -g1 -Isource "
+[ -z "$CFLAGS" ] && CFLAGS="-O1 -g1 -Isource "
 LFLAGS+=" -lcurses "
 
 TARGET_FILE=${1:-moria.c}
 PLATFORM=${2:-unix}
 
-[ -z $PLATFORM ] && PLATFORM="unix"
+[ -z "$PLATFORM" ] && PLATFORM="unix"
 FILE_LIST=`ls -f1 $PLATFORM/*c source/*.c` || exit 1
 
 [ ! -f moria.c ] && printf "#include \"%s\"\n" $FILE_LIST > moria.c
